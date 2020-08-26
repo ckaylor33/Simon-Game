7238eb7564a284ce9ad728a5efcedf04fa2e79cc
Charlie Kaylor <68200523+ckaylor33@users.noreply.github.com>
2020-08-26 22:11:11 +0100

    Add mid-way project files.

---
+++ game.js
@@ -0,0 +1,22 @@
+var userClickedPattern = [];
+
+var buttonColours = [`red`, `blue`, `green`, `yellow`];
+
+var gamePattern = [];
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  var audio = new Audio(`sounds/${randomChosenColour}.mp3`);
+  audio.play();
+}
+
+$(`.btn`).click(function () {
+  var userChosenColour = $(this).attr(`id`);
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+});

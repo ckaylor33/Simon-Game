a41aabe83109c15f29fafc73a251b67e29fd031e
Charlie Kaylor <68200523+ckaylor33@users.noreply.github.com>
2020-08-27 06:26:40 +0100

    Update to JS file

--- game.js
+++ game.js
@@ -4,19 +4,28 @@ var buttonColours = [`red`, `blue`, `green`, `yellow`];
 
 var gamePattern = [];
 
+//user click detection
+
+$(`.btn`).click(function () {
+  var userChosenColour = $(this).attr(`id`);
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+});
+
+//Next sequence for the user
+
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
   $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100);
-
-  var audio = new Audio(`sounds/${randomChosenColour}.mp3`);
-  audio.play();
+  playSound(randomChosenColour);
 }
 
-$(`.btn`).click(function () {
-  var userChosenColour = $(this).attr(`id`);
-  userClickedPattern.push(userChosenColour);
-  console.log(userClickedPattern);
-});
+//play sound for next sequence and click
+
+function playSound(name) {
+  var audio = new Audio(`sounds/${name}.mp3`);
+  audio.play();
+}

93c9de8a3f7c3dc2ce9444436df88f7cfd0f516d
Charlie Kaylor <68200523+ckaylor33@users.noreply.github.com>
2020-09-01 11:33:43 +0100

    JS update

--- game.js
+++ game.js
@@ -4,6 +4,45 @@ var buttonColours = [`red`, `blue`, `green`, `yellow`];
 
 var gamePattern = [];
 
+var started = false;
+
+var level = 0;
+
+checkAnswer(userClickedPattern.length - 1);
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log(`success`);
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log(`wrong`);
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    $(`#level-title`).text(`Game Over, Press Any Key to restart`);
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
+
+$(document).keypress(function () {
+  if (!started) {
+    $(`#level-title`).text(`Level ${level}`);
+    nextSequence();
+    started = true;
+  }
+});
+
 //user click detection
 
 $(`.btn`).click(function () {
@@ -16,6 +55,9 @@ $(`.btn`).click(function () {
 //Next sequence for the user
 
 function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $(`#level-title`).text(`Level ${level}`);
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
